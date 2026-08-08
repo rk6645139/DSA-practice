174e9086d51defb1f95e183f2f0c6c9a3aebcb91
rk6645139 <144534523+rk6645139@users.noreply.github.com>
2026-08-09 02:25:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (55.19%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
 int solve(int m, int n, vector<vector<int>>&dp, int i, int j){
+    
     if(i>=m)return 0;
     if(j>=n)return 0;
     if(i==m-1 &&j==n-1)return 1;
@@ -10,6 +11,7 @@ int solve(int m, int n, vector<vector<int>>&dp, int i, int j){
     dp[i][j]=down+right;
     return dp[i][j];
 }
+
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
         return solve(m,n, dp, 0, 0);
